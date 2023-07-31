b2bf0e9f67f032606577370923779fa39ef15e73
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-07-31 13:41:34 +0530

    Added solution - LeetHub

---
+++ Display longest name - GFG/display-longest-name.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.Scanner;
+
+  public class longestString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t;
+
+        t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n;
+            n = sc.nextInt();
+            String[] names = new String[n];
+
+            for (int j = 0; j < n; j++) names[j] = sc.next();
+
+            GFG obj = new GFG();
+            System.out.println(obj.longest(names, n));
+        }
+    }
+
+}
+
+// } Driver Code Ends
+
+
+
+
+class GFG {
+    String longest(String names[], int n) {
+        String longName = "";
+        
+        for(int i=0;i<n;i++)
+        {
+            String name = names[i];
+            if(name.length() > longName.length())
+            {
+              
+                longName = names[i];
+            }
+            
+        }
+        return longName;
+    }
+}
