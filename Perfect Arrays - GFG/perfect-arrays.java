2d7d9db333c94d8733d0dd97b62958f8b0b32c0a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-08-01 10:03:16 +0530

    Added solution - LeetHub

---
+++ Perfect Arrays - GFG/perfect-arrays.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    int n = Integer.parseInt(br.readLine());
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    if(obj.IsPerfect(arr, n))
+		        System.out.println("PERFECT");
+		    else
+		        System.out.println("NOT PERFECT");
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+        int j = a.length-1;
+        // Complete the function
+        for(int i = 0;  i<=a.length/2; i++){
+            if(i != j){
+                if(a[i]!=a[j]){
+                    return false;
+                }
+                j--;
+            }
+            
+        }
+        return true;
+    }
+    
+}
+
