7b60533f3528c7a058c1492166511af2847d3fa0
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-08-24 17:56:02 +0530

    Time: 1 ms (46.76%) | Memory: 44.3 MB (77.93%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -1,37 +1,26 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        
-        //Storing the values of mat matrix
-        //i.e m = rows & n = cols
-        int rows = mat.length;
-        int cols = mat[0].length;
-        
-        //if the product of rows & cols of mat matrix and the new matrix are not same then return original matrix
-        if((rows * cols) != (r * c)) return mat;
-        
-        //Creating the new matrix
-        int[][] output = new int[r][c];
-        int output_rows = 0;
-        int output_cols = 0;
-        
-        
-        //Traversing the mat matrix and storing the its values in new matrix output cols wise
-        for(int i = 0; i < rows; i++)
-        {
-            for(int j = 0; j < cols; j++)
-            {
-                output[output_rows][output_cols] = mat[i][j];
-                output_cols++;
-                
-                //if the cols value reached then change the row and set the cols value to 0.
-                if(output_cols == c)
-                {
-                    output_cols = 0;
-                    output_rows++;
+
+        int[][] array = new int[r][c];
+        int k = 0;
+        int l = 0;
+
+        if(mat.length * mat[0].length != r * c){
+            return mat;
+        }
+
+        for(int i = 0; i<mat.length; i++){
+            for(int j =0; j<mat[i].length; j++){
+                array[k][l] = mat[i][j];
+                l++;
+                if(l == c){
+                    l=0;
+                    k++;
                 }
+               
+               
             }
         }
-        
-        return output;
+        return array;
     }
 }
