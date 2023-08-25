b7f53c0ab28686aa7fd583e90cb26d19ec87d9b4
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-08-25 12:20:56 +0530

    Time: 13 ms (95.43%) | Memory: 43.8 MB (97.28%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -1,21 +1,59 @@
 class Solution {
-   public static int findLHS(int[] nums) {
-        int max = 0;
+//    public static int findLHS(int[] nums) {
+//         int max = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             int count = 0;
+//             int count1 = 0;
+//             for (int j = 0; j < nums.length; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     count++;
+//                 }
+//                 if (nums[i] + 1 == nums[j]) {
+//                     count1++;
+//                 }
+//             }
+//             if (count1 != 0) {
+//                 max = Math.max(max, count + count1);
+//             }
+//         }
+//         return max;
+//     }
+
+  public static int findLHS(int[] nums){
+        Arrays.sort(nums);
+        int max =0;
+        int count = 0; int count1 = 0; int start =0;
         for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            int count1 = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
+            if (Math.abs(nums[start] - nums[i]) < 2 ){
+                if (nums[start] - nums[i] == 0){
                     count++;
-                }
-                if (nums[i] + 1 == nums[j]) {
+                }else {
                     count1++;
                 }
+            }else {
+                if (count != 0 && count1 !=0){
+                    max = Math.max(max, count + count1);
+                }
+                if (nums[i]  - nums[i-1] ==1){
+                    start = i-1;
+                    count = count1;
+                    count1 =1;
+                }else {
+                    start = i;
+                    count = 1;
+                    count1 =0;
+                }
+
+
             }
-            if (count1 != 0) {
-                max = Math.max(max, count + count1);
-            }
+
+
+
+        }
+        if (count != 0 && count1 !=0){
+            max = Math.max(max, count + count1);
         }
         return max;
+
     }
 }
