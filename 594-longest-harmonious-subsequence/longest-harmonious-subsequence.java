c58bb45dc18f2dc0c60eb5f5d616462bd1f80caf
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-08-25 13:53:48 +0530

    Time: 19 ms (53.03%) | Memory: 44.9 MB (19.04%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -19,41 +19,64 @@ class Solution {
 //         return max;
 //     }
 
-  public static int findLHS(int[] nums){
-        Arrays.sort(nums);
-        int max =0;
-        int count = 0; int count1 = 0; int start =0;
-        for (int i = 0; i < nums.length; i++) {
-            if (Math.abs(nums[start] - nums[i]) < 2 ){
-                if (nums[start] - nums[i] == 0){
-                    count++;
-                }else {
-                    count1++;
-                }
-            }else {
-                if (count != 0 && count1 !=0){
-                    max = Math.max(max, count + count1);
-                }
-                if (nums[i]  - nums[i-1] ==1){
-                    start = i-1;
-                    count = count1;
-                    count1 =1;
-                }else {
-                    start = i;
-                    count = 1;
-                    count1 =0;
-                }
+//   public static int findLHS(int[] nums){
+//         Arrays.sort(nums);
+//         int max =0;
+//         int count = 0; int count1 = 0; int start =0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (Math.abs(nums[start] - nums[i]) < 2 ){
+//                 if (nums[start] - nums[i] == 0){
+//                     count++;
+//                 }else {
+//                     count1++;
+//                 }
+//             }else {
+//                 if (count != 0 && count1 !=0){
+//                     max = Math.max(max, count + count1);
+//                 }
+//                 if (nums[i]  - nums[i-1] ==1){
+//                     start = i-1;
+//                     count = count1;
+//                     count1 =1;
+//                 }else {
+//                     start = i;
+//                     count = 1;
+//                     count1 =0;
+//                 }
 
 
+//             }
+
+
+
+//         }
+//         if (count != 0 && count1 !=0){
+//             max = Math.max(max, count + count1);
+//         }
+//         return max;
+
+//     }
+public static int findLHS(int[] nums) {
+        int max = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (var num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.getOrDefault(num, 1) + 1);
+            } else {
+                map.put(num, 1);
             }
-
-
-
         }
-        if (count != 0 && count1 !=0){
-            max = Math.max(max, count + count1);
+
+        for (var num : nums) {
+            int num1 = 0;
+            int nu2 = 0;
+            num1 = map.get(num);
+            int num2 = num + 1;
+            if (map.containsKey(num2)) {
+                nu2 = map.get(num2);
+                max = Math.max(max, num1 + nu2);
+            }
         }
         return max;
-
     }
 }
