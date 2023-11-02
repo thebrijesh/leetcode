e35ca919662b249c73316cbd76236c7f16b2b63c
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-02 11:30:16 +0530

    Time: 7 ms (23.94%) | Memory: 84.4 MB (36.04%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,30 +1,23 @@
 class Solution {
-    private static int finded(boolean idx, int[] nums, int i){
-        if(idx){
-             while(i < nums.length && nums[i] < 0 ){
-                i++;
-             }
-        }else{
-            while(i < nums.length && nums[i] > 0 ){
-                i++;
-            }
-        }
-        return i;
-    } 
+
     public int[] rearrangeArray(int[] nums) {
         int[] arr = new int[nums.length];
         boolean idx = true;
-        int i=-1;
-        int j=-1;
+        int i=0;
+        int j=0;
         for(int k = 0; k < arr.length; k++){
             if(idx){
-                i = finded(idx,nums,i+1);
+                 while(i < nums.length && nums[i] < 0 ){
+                  i++;
+                 }
                 idx = !idx;
-                arr[k] = nums[i];
+                arr[k] = nums[i++];
             }else{
-                j = finded(idx,nums,j+1);
+                while(j < nums.length && nums[j] > 0 ){
+                  j++;
+                }
                 idx = !idx;
-                arr[k] = nums[j];
+                arr[k] = nums[j++];
             }
            
 
