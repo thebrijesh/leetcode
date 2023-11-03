5ad795c69d4dbe064fd1ac6aa660c227b2ca67c7
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-03 17:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (47.47%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.java
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -7,9 +7,11 @@ class Solution {
             if(nums[i] > nums[i+1] ){
                 
                 chance--;
-                if(chance < 0 || (i >= 1 && i < nums.length-2 && nums[i-1 ] > nums[i+1] && nums[i] > nums[i+2])) {
-
-                    return false;
+                if(chance < 0)return false;
+                if(i >= 1 && nums[i-1] > nums[i+1]){
+                    nums[i+1] = nums[i];
+                }else{
+                    nums[i] = nums[i + 1];
                 }
 
 
