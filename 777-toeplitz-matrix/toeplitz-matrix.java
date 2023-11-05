8ab2e9a99f398d896ef3f7279a57cafc2bfb95b2
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-05 11:21:23 +0530

    Time: 1 ms (45.91%) | Memory: 42.9 MB (71.33%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,12 +1,1 @@
-class Solution {
-    public boolean isToeplitzMatrix(int[][] nums) {
-        for(int i = 0; i < nums.length-1; i++){
-            for(int j = 0; j < nums[i].length-1; j++){
-                if(nums[i][j] != nums[i+1][j+1]){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
+class Solution {public boolean isToeplitzMatrix(int[][] nums) {for(int i = 0; i < nums.length-1; i++){for(int j = 0; j < nums[i].length-1; j++){if(nums[i][j] != nums[i+1][j+1])return false;}}return true;}}
