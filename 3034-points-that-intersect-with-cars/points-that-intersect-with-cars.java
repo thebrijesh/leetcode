f84d39aac5b25dfe8ecda45eda1893012ee57b42
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-09 15:28:21 +0530

    Time: 6 ms (17.49%) | Memory: 43.4 MB (45.34%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,13 +1,6 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        Collections.sort(nums, new Comparator<List<Integer>>() {
-    @Override
-    public int compare(List<Integer> list1, List<Integer> list2) {
-        // Compare the first elements of the two inner lists in descending order
-        return list1.get(0).compareTo(list2.get(0));
-    }
-});
-       
+    
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.size(); i++){
             for(int j = nums.get(i).get(0); j <= nums.get(i).get(1); j++){
