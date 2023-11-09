85300adff53090468a4e355aa9e16a7923d98491
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-09 15:27:15 +0530

    Time: 9 ms (8.68%) | Memory: 42.8 MB (97.14%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -18,6 +18,12 @@ class Solution {
                 max = nums.get(i).get(1);
             }
         }
-        return count;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = nums.get(i).get(0); j <= nums.get(i).get(1); j++){
+                set.add(j);
+            }
+        }
+        return set.size();
     }
 }
