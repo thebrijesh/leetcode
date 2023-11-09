e0da4d4fa5d0f106a67a0025d0239bc70a36aeaa
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-09 15:27:47 +0530

    Time: 10 ms (7.61%) | Memory: 42.7 MB (98.70%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -7,17 +7,7 @@ class Solution {
         return list1.get(0).compareTo(list2.get(0));
     }
 });
-        int count = (nums.get(0).get(1) - nums.get(0).get(0))+1;
-        int max = nums.get(0).get(1);
-        for(int i = 1; i < nums.size(); i++){
-            if(nums.get(i).get(0) <= max && max < nums.get(i).get(1)){
-                count += nums.get(i).get(1) - max;
-                max = nums.get(i).get(1);
-            }else if(nums.get(i).get(0) > max){
-                count += (nums.get(i).get(1) - nums.get(i).get(0))+1;
-                max = nums.get(i).get(1);
-            }
-        }
+       
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.size(); i++){
             for(int j = nums.get(i).get(0); j <= nums.get(i).get(1); j++){
