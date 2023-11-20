1c5e15a4ce96f2d6d60350716261013d55a8352a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-20 10:11:37 +0530

    Time: 68 ms (63.97%) | Memory: 44.2 MB (8.08%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,16 +1,36 @@
 class Solution {
-  public static int[] minOperations(String boxes) {
+public static int[] minOperations(String boxes) {
         int[] arr = new int[boxes.length()];
 
+        int count = 0;
+        int sum =0;
+        int idx = 0;
+        boolean flag = false;
         for (int i = 0; i < boxes.length(); i++) {
-            int ans = 0;
-            for (int j = 0; j < boxes.length(); j++) {
-                if (i != j && boxes.charAt(j) == '1') {
-                    ans += Math.abs(j - i);
-                }
+            arr[i] += sum;
+            if(boxes.charAt(i) == '1'){
+                count++;
+
+
             }
-            arr[i] = ans;
+            sum += count;
+            
         }
+        sum = 0; count = 0;
+
+        for (int i = boxes.length()-1; i >= 0; i--) {
+            arr[i] += sum;
+            if(boxes.charAt(i) == '1'){
+                count++;
+
+
+            }
+            sum += count;
+
+        }
+
+
+
         for (var num : arr) {
             System.out.println(num);
         }
