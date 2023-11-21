a90181ee6555a27520152fabba3461aef534f702
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-21 10:06:39 +0530

    Time: 2 ms (100.00%) | Memory: 87.7 MB (11.66%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,18 +1,18 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        long ti = 0;
-        int pr = -1;
+        long tiiiiiiiiiii = 0;
+        int priiiiiiiiii = -1;
         for(int[] c : customers){
             int a = c[0];
             int t = c[1];
-            if(pr <= a){
-                pr = a;
+            if(priiiiiiiiii <= a){
+                priiiiiiiiii = a;
             }
             
-            ti += (pr + t - a);
-            pr += t;
+            tiiiiiiiiiii += (priiiiiiiiii + t - a);
+            priiiiiiiiii += t;
         }
         
-        return (double) ti / customers.length;
+        return (double) tiiiiiiiiiii / customers.length;
     }
 }
