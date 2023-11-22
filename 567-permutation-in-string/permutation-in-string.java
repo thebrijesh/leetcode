b2875ce30f80ce257429337596029a425e4c1b23
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-22 14:03:28 +0530

    Time: 1192 ms (5.00%) | Memory: 43.9 MB (18.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
- public static boolean checkInclusion(String s1, String s2) {
-     int n = s1.length();
+public static boolean checkInclusion(String s1, String s2) {
+       int n = s1.length();
 
         if (n > s2.length()) return false;
 
@@ -10,7 +10,7 @@ class Solution {
             map.put(s1.charAt(i), map.getOrDefault(s1.charAt(i), 0) + 1);
         }
 
-        for (int i = 0; i <= s2.length() - n; i++) {
+        for (int i = 0; i <= s2.length()-n; i++) {
             HashMap<Character, Integer> map2 = new HashMap<>(map);
 
             int j = 0;
