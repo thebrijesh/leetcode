fb921076018e4218bb3614a4742a87ab32882a06
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-24 13:53:08 +0530

    Time: 34 ms (59.32%) | Memory: 40.7 MB (71.75%) - LeetSync

---
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int secondsToRemoveOccurrences(String s) {
+        char[] ch = new char[s.length()];
+        int count = 0;
+        ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            boolean check = false;
+            for (int j = 0; j < ch.length-1; j++) {
+                if(ch[j] == '0' && ch[j+1] == '1'){
+                    ch[j] = '1';
+                    ch[j+1] = '0';
+                    j+= 1;
+                    check = true;
+                }
+            }
+            if (check)count++;
+        }
+        return count;
+    }
+}
