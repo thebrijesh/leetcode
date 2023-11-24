09d2c0d287db31b039d36adc8b2311e1fad3cbae
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-11-24 10:48:52 +0530

    Time: 14 ms (92.95%) | Memory: 44.1 MB (19.82%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.java
@@ -1,19 +1,20 @@
 class Solution {
     public static int[] executeInstructions(int n, int[] nums, String s) {
+        char[] charr = s.toCharArray();
         int[] arr = new int[s.length()];
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < charr.length; i++) {
             int raw = nums[0];
             int cal = nums[1];
             int count = 0;
-            for (int j = i; j < s.length(); j++) {
-                if(s.charAt(j) == 'R'){
+            for (int j = i; j < charr.length; j++) {
+                if(charr[j] == 'R'){
                     cal++;
 
-                } else if(s.charAt(j) == 'D'){
+                } else if(charr[j] == 'D'){
                     raw++;
-                }else if(s.charAt(j) == 'L'){
+                }else if(charr[j] == 'L'){
                     cal--;
-                }else if(s.charAt(j) == 'U'){
+                }else if(charr[j] == 'U'){
                     raw--;
                 }
                 if(raw >= 0 && cal >= 0 && raw < n && cal < n){
