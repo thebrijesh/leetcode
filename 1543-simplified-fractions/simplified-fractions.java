488c41966055e644edce04039fe82e946ac368f4
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-12-06 16:43:48 +0530

    Time: 18 ms (78.13%) | Memory: 45.2 MB (5.47%) - LeetSync

--- 1543-simplified-fractions/simplified-fractions.java
+++ 1543-simplified-fractions/simplified-fractions.java
@@ -1,20 +1,20 @@
 class Solution {
-public static List<String> simplifiedFractions(int n) {
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<Float> list1 = new ArrayList<>();
-        float ans = 0.0F;
-        for (int i = 1; i <=n; i++) {
-            for (int j = 1; j <=n; j++) {
-                ans = (float)i / j;
-                if (i / j < 1 && !list1.contains(ans)){
-                    list.add(String.valueOf(i)+ "/"+String.valueOf(j));
-
-                    list1.add(ans);
+ public static List<String> simplifiedFractions(int n) {
+        List<String> l = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (gcd(i, j) == 1) {
+                    String s = "" + j + "/" + i;
+                    l.add(s);
                 }
             }
-
         }
-        return list;
+        return l;
+    }
+
+    public static int gcd(int i, int j) {
+        if (j == 0) return i;
+        return gcd(j, i % j);
     }
 
 }
