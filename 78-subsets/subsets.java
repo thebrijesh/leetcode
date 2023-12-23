f59d50a03a39b27c165e5d815e911a43c40645a2
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2023-12-23 14:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (7.17%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int idx = 0;
+        solve(list,idx,nums);
+        return ans;
+    }
+
+    public void solve(ArrayList<Integer> list, int idx, int[] nums){
+        if(idx == nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        // exclude
+        solve(list,idx+1,nums);
+
+        //include
+        list.add(nums[idx]);
+        solve(list,idx+1,nums);
+        list.remove(list.size()-1);
+    }
+}
