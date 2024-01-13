66dbad3808ca901ba3a7cf3ebfeab20398a5e181
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-01-13 10:20:43 +0530

    Time: 3 ms (38.80%) | Memory: 44.2 MB (62.44%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        
+        Arrays.sort(candidates);
+        combinationSum(candidates, target, list, new ArrayList<>(), 0, 0);
+        return list;
+    }
+
+    public void combinationSum(int[] candidates, int target, List<List<Integer>> list2d, List<Integer> list, int start, int sum) {
+        if (sum > target) return;
+        else if (sum == target) list2d.add(new ArrayList<>(list));
+        else {
+            for (int i = start; i < candidates.length; i++) {
+                list.add(candidates[i]);
+                combinationSum(candidates, target, list2d, list, i, sum + candidates[i]);
+                list.remove(list.size() - 1);
+            }
+        }
+
+
+    }
+
+}
