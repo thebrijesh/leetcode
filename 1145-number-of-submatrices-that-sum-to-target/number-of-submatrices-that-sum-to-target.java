2128d57fcc56179d7b564477f4c71a307eb23df3
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-01-28 11:36:47 +0530

    Time: 134 ms (54.34%) | Memory: 44.6 MB (64.74%) - LeetSync

---
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public static int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+
+
+    }
+    public static int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int count = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            
+            int[] sum = new int[matrix[0].length];
+            for (int j = i; j < matrix.length; j++) {
+                for (int k = 0; k < matrix[0].length; k++) {
+                    sum[k] += matrix[j][k];
+                }
+                count += subarraySum(sum, target);
+            }
+        }
+        return count;
+    }
+
+}
