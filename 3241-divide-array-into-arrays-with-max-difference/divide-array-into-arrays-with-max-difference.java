9a3a73b15dd39b4d8bfc0234ffa2ef1248f2860b
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-02-01 10:41:41 +0530

    Time: 24 ms (68.69%) | Memory: 59.4 MB (10.03%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -3,17 +3,20 @@ class Solution {
         int ans[][]=new int[nums.length/3][3];
         int ind=0;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i+=3){
-
+        for(int i=0;i<nums.length;i+=3)
+        {
             int first=nums[i];
             int second=nums[i+1];
             int third=nums[i+2];
-            if(third-first <= k){
+            if(third-first <= k)
+            {
                 ans[ind][0]=first;
                 ans[ind][1]=second;
                 ans[ind][2]=third;
                 ind++;
-            }else{
+            }
+            else
+            {
                 return new int[0][0];
             }
         }
