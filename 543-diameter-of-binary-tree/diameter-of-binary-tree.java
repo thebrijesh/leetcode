2521c02073c09f3e6fa43308a7dc0c3f7b18dd3a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-02-15 11:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.04%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,36 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public int height(TreeNode root) {
-
-        if (root == null || (root.left == null && root.right == null))
-            return 0;
-
-        return 1 + Math.max(height(root.left), height(root.right));
+    int result = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        helper(root);
+        return result;
     }
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null))
+    private int helper(TreeNode root) {
+        if (root == null) {
             return 0;
-        int leftAns = diameterOfBinaryTree(root.left);
-        int rightAns = diameterOfBinaryTree(root.right);
-        int mid = height(root.left) + height(root.right);
-        if(root.right != null) mid++;
-        if(root.left != null) mid++;
-        int max = Math.max(leftAns, Math.max(rightAns,mid));
-        return max;
+        }
+
+        int left = helper(root.left);
+        int right = helper(root.right);
+
+        result = Math.max(result, left + right);
+
+        return 1+ Math.max(left, right);
     }
 }
