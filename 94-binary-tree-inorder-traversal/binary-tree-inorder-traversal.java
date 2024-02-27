504b4304fd5887f16b581846aa190bd3f95318d7
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-02-27 18:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (66.52%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,35 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
 
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        if(root==null)return list;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode temp = st.peek();
-            if(temp.left != null){
-                st.push(temp.left);
-                temp.left = null;
-            }
-            else{
-                st.pop();
-                list.add(temp.val);
-                if(temp.right != null)st.push(temp.right);
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr) {
+                    pred = pred.right;
+                }
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                if (pred.right == curr) {
+                    pred.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
+                }
+            } else {
+
+                list.add(curr.val);
+                curr = curr.right;
+
             }
         }
         return list;
