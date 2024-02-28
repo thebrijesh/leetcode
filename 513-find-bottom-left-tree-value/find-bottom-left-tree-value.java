15dc9db977ab32276aec3a7064f0d23a1a63d5b2
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-02-28 10:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (83.03%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -1,44 +1,49 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     int value = 0;
     boolean v = true;
-    public  int height(TreeNode root) {
-        if (root == null) return 0;
 
-        if (root.left == null && root.right == null) return 0;
-        return 1 + Math.max(height(root.left),height(root.right));
+    public int height(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        if (root.left == null && root.right == null)
+            return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
     }
-    public  void nthLevel(TreeNode root, int n){
-        if(root == null)return;
-        if (n==1 && v){
-            System.out.println(root.val + " ");
-            
+
+    public void nthLevel(TreeNode root, int n) {
+        if (root == null)
+            return;
+        if (n == 1 && v) {
+
             value = root.val;
             v = false;
             return;
         }
-        nthLevel(root.left,n-1);
-        nthLevel(root.right,n-1);
-        
+        nthLevel(root.left, n - 1);
+        nthLevel(root.right, n - 1);
+
     }
+
     public int findBottomLeftValue(TreeNode root) {
-        int val = height(root)+1;
+        int val = height(root) + 1;
         // System.out.println(val + " ");
-         nthLevel(root,val);
-         return value;
+        nthLevel(root, val);
+        return value;
     }
 }
