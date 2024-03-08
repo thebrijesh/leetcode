373da75299eb9a9461cc506c5d4a9c442b466f7a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-03-08 15:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (40.21%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -2,11 +2,14 @@ class Solution {
     public int fib(int n) {
         if(n == 1)return 1;
         int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
         fib(n,dp);
         return dp[n];
     }
     public int fib(int n, int[] dp){
         if(n <= 1)return n;
+        if(dp[n] != -1)return dp[n];
         dp[n] = fib(n-1,dp) + fib(n-2,dp);
         return dp[n];
     }
