74c124587335dd2389b24a764a65a2d85adcced2
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-03-09 08:22:57 +0530

    Time: 0 ms (100.00%) | Memory: 62.4 MB (69.04%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,39 +1,51 @@
+
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-    int i = 0, j = 0;
-    while(i < nums1.length && j < nums2.length ){
-        if(nums1[i] == nums2[j]) return nums1[i];
-        if(nums1[i] > nums2[j]) j++;
-        else i++;
+        int i = 0, j = 0;
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        // For faster solution
+        if (nums1[length1 - 1] < nums2[0] || nums2[length2 - 1] < nums1[0])
+            return -1;
+
+        // Two pointer approach
+        while (i < length1 && j < length2) {
+            if (nums1[i] == nums2[j])
+                return nums1[i];
+            else if (nums1[i] > nums2[j])
+                j++;
+            else
+                i++;
+        }
+        return -1;
     }
-    return -1;
-}
+
     // public int getCommon(int[] nums1, int[] nums2) {
-    //     for(int i = 0; i < nums1.length; i++){
-    //         boolean check = binarySearch(nums1[i],nums2);
-    //         if(check){
-    //             return nums1[i];
-    //         }
-    //     }
-            
-    //     return -1;
+    // for(int i = 0; i < nums1.length; i++){
+    // boolean check = binarySearch(nums1[i],nums2);
+    // if(check){
+    // return nums1[i];
+    // }
+    // }
+
+    // return -1;
     // }
 
     // public boolean binarySearch(int target, int[] nums2) {
-    //     int start = 0;
-    //     int end = nums2.length;
-    //     while (start > end) {
-    //         int mid = start + (end - start) / 2;
-    //        System.out.print(0);
-    //         if (nums2[mid] == target)
-    //             return true;
-    //         else if (nums2[mid] > target)
-    //             start = mid + 1;
-    //         else
-    //             end = mid - 1;
+    // int start = 0;
+    // int end = nums2.length;
+    // while (start > end) {
+    // int mid = start + (end - start) / 2;
+    // System.out.print(0);
+    // if (nums2[mid] == target)
+    // return true;
+    // else if (nums2[mid] > target)
+    // start = mid + 1;
+    // else
+    // end = mid - 1;
 
-    //     }
-        
-    //     return false;
+    // }
+
+    // return false;
     // }
 }
