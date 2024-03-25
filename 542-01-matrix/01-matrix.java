a175412d10a991161118d76d1d135b1462d3cdd2
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-03-25 10:39:18 +0530

    Time: 15 ms (45.84%) | Memory: 47 MB (73.77%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[0].length; j++){
+                if(mat[i][j] == 0){
+                    int[] temp = {i , j};
+                    queue.add(temp);
+                }else{
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        while(!queue.isEmpty()){
+            int i = queue.peek()[0];
+            int j = queue.peek()[1];
+            // if(i > 0 && j > 0 && i < mat.length-1 && j < mat[0].length-1){
+            //     int left = mat[i][j-1];
+            // }
+
+            if(j > 0 && mat[i][j-1] == -1){
+                mat[i][j-1] = mat[i][j]+1;
+                int[] temp = {i,j-1};
+                queue.add(temp);
+            }
+
+            if( j < mat[0].length-1 && mat[i][j+1] == -1){
+                mat[i][j+1] = mat[i][j]+1;
+                int[] temp = {i , j+1};
+                queue.add(temp);
+            }
+            
+            if(i > 0 && mat[i-1][j] == -1){
+                mat[i-1][j] = mat[i][j]+1 ;
+                int[] temp = {i-1 , j};
+                queue.add(temp);
+            }
+
+            if( i < mat.length-1 && mat[i+1][j] == -1){
+                mat[i+1][j] = mat[i][j]+1;
+                int[] temp = {i+1 , j};
+                queue.add(temp);
+            }
+
+            queue.remove();
+        }
+        return mat;
+    }
+}
