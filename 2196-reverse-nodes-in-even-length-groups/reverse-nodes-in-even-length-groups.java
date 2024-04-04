eb0fc29b5a5ccebce673e17d728b9c5298de6bf9
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-04 09:34:37 +0530

    Time: 5 ms (94.68%) | Memory: 67.4 MB (39.75%) - LeetSync

--- 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.java
+++ 2196-reverse-nodes-in-even-length-groups/reverse-nodes-in-even-length-groups.java
@@ -1,47 +1,58 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode reverseEvenLengthGroups(ListNode head) {
-          ListNode temp = head;
-    int num=1;
-    
-    while(temp!=null)
-    {
-        int count=0;
-        ListNode start = temp;
-        Stack st = new Stack();
-        
-        while(count!=num && temp!=null)
-        {
-            st.push(temp.val);
-            temp=temp.next;
-            count++;
-                
+    private ListNode reverse(ListNode head, int count) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null && count > 0) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+
+            // EXTRA CASE ADDS UP...
+            count--;
         }
-        
-        if(count%2==0)
-        {
-            while(temp!=start)
-            {
-              start.val=(int) st.pop();
-             start=start.next;  
-            }
-            
-        }
-        
-        num++;
+        head.next = next;
+        return prev;
     }
-    
-    
-    return head;
-    
+
+
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        // only the swaping is possible
+        // BASIC START POINT
+        int counter = 2;
+
+        ListNode cur = head.next;
+        ListNode prev = head;
+        while (cur != null) {
+            ListNode l_curr = cur; // copy of the head.next
+            ListNode l_prev = prev; // copy of the head
+            int count = 0;
+            while (cur != null && count < counter) {
+                prev = cur;
+                cur = cur.next;
+                count++;
+            }
+
+            // if even number then REVERSE IT...
+            // increase the counter...
+            if (Math.min(count, counter) % 2 == 0) {
+                ListNode newHead = reverse(l_curr, Math.min(count, counter));
+                l_prev.next = newHead;
+                prev = l_curr;
+            }
+            counter++;
+        }
+        return head;
     }
 }
