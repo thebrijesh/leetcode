0995b99fd354ee454ae97b4b2357ea4b9aa9e72a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-12 08:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (11.36%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int totalWater = 0;
-        int n = height.length;
-
-        for (int i = 0; i < n; i++) {
-            int leftMax = 0;
-            int rightMax = 0;
-
-            // Find the maximum height on the left side (including the current bar)
-            for (int j = i; j >= 0; j--) {
-                leftMax = Math.max(leftMax, height[j]);
+        int l = 0, r = height.length - 1, sum = 0, leftBar = height[l], rightBar = height[r];
+        
+        while (l < r) {
+            if (leftBar <= rightBar) {
+                sum += leftBar - height[l];
+                l++;
+                leftBar = Math.max(leftBar, height[l]);
+            } else {
+                sum += rightBar - height[r];
+                r--;
+                rightBar = Math.max(rightBar, height[r]);
             }
-
-            // Find the maximum height on the right side (including the current bar)
-            for (int j = i; j < n; j++) {
-                rightMax = Math.max(rightMax, height[j]);
-            }
-
-            // Calculate the amount of water trapped at the current bar
-            int trappedWater = Math.min(leftMax, rightMax) - height[i];
-
-            // Add the trapped water to the total water
-            totalWater += trappedWater;
         }
-        return totalWater;
+
+        return sum;
     }
 }
