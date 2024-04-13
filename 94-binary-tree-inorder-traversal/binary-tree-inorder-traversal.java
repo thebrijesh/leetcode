f904bf3bfdcbe41f654fd3dd92cf326fbf757de8
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-13 17:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (23.51%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,31 +15,48 @@
  */
 class Solution {
 
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    // List<Integer> list = new ArrayList<>();
+    // TreeNode curr = root;
+    // while (curr != null) {
+    // if (curr.left != null) {
+    // TreeNode pred = curr.left;
+    // while (pred.right != null && pred.right != curr) {
+    // pred = pred.right;
+    // }
+    // if (pred.right == null) {
+    // pred.right = curr;
+    // curr = curr.left;
+    // }
+    // if (pred.right == curr) {
+    // pred.right = null;
+    // list.add(curr.val);
+    // curr = curr.right;
+    // }
+    // } else {
+
+    // list.add(curr.val);
+    // curr = curr.right;
+
+    // }
+    // }
+    // return list;
+    // }
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        TreeNode curr = root;
-        while (curr != null) {
-            if (curr.left != null) {
-                TreeNode pred = curr.left;
-                while (pred.right != null && pred.right != curr) {
-                    pred = pred.right;
-                }
-                if (pred.right == null) {
-                    pred.right = curr;
-                    curr = curr.left;
-                }
-                if (pred.right == curr) {
-                    pred.right = null;
-                    list.add(curr.val);
-                    curr = curr.right;
-                }
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while (p != null || !stack.isEmpty()) {
+            if (p != null) {
+                stack.push(p);
+                p = p.left;
             } else {
-
-                list.add(curr.val);
-                curr = curr.right;
-
+                TreeNode node = stack.pop();
+                result.add(node.val); // Add after all left children
+                p = node.right;
             }
         }
-        return list;
+        return result;
     }
 }
