05e8a7efdac52a86f8fe1572b31b2edb4098ca8e
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-14 09:49:22 +0530

    Time: 1 ms (10.24%) | Memory: 41.1 MB (72.33%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -1,37 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        Deque<TreeNode> q = new ArrayDeque<>();
+        Queue<TreeNode> q = new LinkedList<>();
         int sum = 0;
-       
+
         TreeNode p = root;
-        while(p != null || !q.isEmpty()){
-            if(p!= null){
-                q.push(p);
-                
-                if(p.left!= null  && (p.left.left == null && p.left.right == null)){
+        while (p != null || !q.isEmpty()) {
+            if (p != null) {
+                q.add(p);
+
+                if (p.left != null && (p.left.left == null && p.left.right == null)) {
                     sum += p.left.val;
                 }
                 p = p.left;
-            }else{
-                
-                TreeNode node = q.pop();
-                
-                
+            } else {
+
+                TreeNode node = q.remove();
+
                 p = node.right;
             }
         }
