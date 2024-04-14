c99f44f38f0c08d9a359ba4cce4aeb88f63d2a99
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-14 09:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (14.30%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -1,36 +1,39 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public int left = 0;
     public int sumOfLeftLeaves(TreeNode root) {
-        // if(root == null) return 0;
-        
-        class Traverse {
-            Traverse(TreeNode root) {
-                TreeNode temp = root;
-                if(temp.left != null) {
-                    if(temp.left.left == null && temp.left.right == null) left += temp.left.val;
-                    new Traverse(temp.left);
+        Queue<TreeNode> q = new LinkedList<>();
+        int sum = 0;
+
+        TreeNode p = root;
+        while (p != null || !q.isEmpty()) {
+            if (p != null) {
+                q.add(p);
+
+                if (p.left != null && (p.left.left == null && p.left.right == null)) {
+                    sum += p.left.val;
                 }
-                if(temp.right != null) {
-                    new Traverse(temp.right);
-                }
+                p = p.left;
+            } else {
+
+                
+
+                p = q.remove().right;
             }
         }
-        new Traverse(root);
-        return left;
+        return sum;
     }
 }
