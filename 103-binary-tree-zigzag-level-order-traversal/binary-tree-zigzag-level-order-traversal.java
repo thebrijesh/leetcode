323240b97bb4794e4c7d23b576be089d37d9d55f
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-15 12:07:04 +0530

    Time: 1 ms (74.94%) | Memory: 41.5 MB (97.53%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,51 +14,80 @@
  * }
  */
 class Solution {
+    // public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    //     List<List<Integer>> list2D = new ArrayList<>();
+    //     int level = 1;
+    //     int count = 1;
+    //     int nextNull = 0;
+    //     int currNull = 0;
+    //     int l = 1;
+
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     if (root != null)
+    //         queue.add(root);
+    //     List<Integer> list = new ArrayList<>();
+    //     while (!queue.isEmpty()) {
+    //         TreeNode temp = queue.peek();
+
+    //         if (temp.left != null) {
+    //             queue.add(temp.left);
+    //         } else {
+    //             nextNull++;
+    //         }
+    //         if (temp.right != null) {
+    //             queue.add(temp.right);
+    //         } else {
+    //             nextNull++;
+    //         }
+    //         if (l % 2 == 0) {
+    //             list.add(0, temp.val);
+    //         } else {
+    //             list.add(temp.val);
+    //         }
+
+    //         if (level == count + currNull) {
+    //             list2D.add(list);
+    //             list = new ArrayList<>();
+    //             level = level * 2;
+    //             count = 0;
+    //             currNull = nextNull;
+    //             nextNull *= 2;
+    //             l++;
+    //         }
+    //         queue.remove();
+    //         count++;
+    //     }
+
+    //     return list2D;
+    // }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> list2D = new ArrayList<>();
-        int level = 1;
-        int count = 1;
-        int nextNull = 0;
-        int currNull = 0;
-        int l = 1;
-
         Queue<TreeNode> queue = new LinkedList<>();
         if (root != null)
             queue.add(root);
-        List<Integer> list = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            TreeNode temp = queue.peek();
+        
+        int level = 0;
+         while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(size-- > 0){
+                if(queue.peek().left != null){
+                    queue.add(queue.peek().left);
+                }
+                if(queue.peek().right != null){
+                    queue.add(queue.peek().right);
+                }
+                if(level % 2 == 0){
+                    list.add(queue.remove().val);
+                }else{
+                    list.add(0,queue.remove().val);
+                }
+                
+            }
 
-            if (temp.left != null) {
-                queue.add(temp.left);
-            } else {
-                nextNull++;
-            }
-            if (temp.right != null) {
-                queue.add(temp.right);
-            } else {
-                nextNull++;
-            }
-            if (l % 2 == 0) {
-                list.add(0, temp.val);
-            } else {
-                list.add(temp.val);
-            }
-
-            if (level == count + currNull) {
-                list2D.add(list);
-                list = new ArrayList<>();
-                level = level * 2;
-                count = 0;
-                currNull = nextNull;
-                nextNull *= 2;
-                l++;
-            }
-            queue.remove();
-            count++;
-        }
-
-        return list2D;
+            list2D.add(list);
+            level++;
+         }
+return list2D;
     }
-
 }
