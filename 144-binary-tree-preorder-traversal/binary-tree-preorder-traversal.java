543eacc6833b32c66b2d2ff06be2e2aa1a58d900
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-15 09:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (72.81%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,33 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode p = root;
-        while (p != null || !stack.isEmpty()) {
-            if (p != null) {
-                stack.push(p);
-                result.add(p.val); // Add before going to children
-                p = p.left;
-            } else {
-                TreeNode node = stack.pop();
-                p = node.right;
+        ArrayList<Integer> arrl = new ArrayList<>();
+        if(root == null) return arrl;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+            arrl.add(current.val);
+            if(current.right != null){
+                stack.push(current.right);
+            }
+            if(current.left != null){
+                stack.push(current.left);
             }
         }
-        return result;
+        return arrl;
     }
 }
