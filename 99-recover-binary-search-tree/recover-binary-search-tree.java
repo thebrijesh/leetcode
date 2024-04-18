b98b5747b9a0b991ea2043acffc5d971d159f2b3
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-18 14:03:23 +0530

    Time: 32 ms (5.32%) | Memory: 44.6 MB (19.98%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        TreeNode curr = root, first = null, second = null;
+        TreeNode pre = null;
+        while (curr != null) {
+
+            System.out.println(curr.val);
+            if (curr.left == null) {
+                if (pre != null  && curr.val < pre.val) {
+                        if (first == null) {
+                            first = pre;
+                        }
+                        second = curr;
+                    }
+                    pre = curr;
+                curr = curr.right;
+            } else {
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr) {
+                    pred = pred.right;
+                }
+
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+
+                if (pred.right == curr) {
+                    pred.right = null;
+                    if (pre != null  && curr.val < pre.val) {
+                        if (first == null) {
+                            first = pre;
+                        }
+                        second = curr;
+                    }
+                    pre = curr;
+                    curr = curr.right;
+                }
+            }
+
+        }
+        int temp = second.val;
+        second.val = first.val;
+        first.val = temp;
+    }
+
+}
