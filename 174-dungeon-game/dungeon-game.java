00ed516ac79432de0f83bc0b848ad1abf04d8ab6
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-04-28 11:20:23 +0530

    Time: 4 ms (6.22%) | Memory: 43.6 MB (98.14%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,30 +1,35 @@
 class Solution {
-    public int calculateMinimumHP(int[][] arr) {
-        
-       int[][] dp = new int[arr.length][arr[0].length];
-		for (int[] a1 : dp) {
-			Arrays.fill(a1, -1);
-		}
-		return (calculateMinimum(arr, 0, 0, dp));
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
 
-	}
+        dungeon[m - 1][n - 1] = Math.max(1 - dungeon[m - 1][n - 1], 1);
+        // for (int i = m - 2; i >= 0; i--) {
+        //     dungeon[i][n - 1] = Math.max(dungeon[i + 1][n - 1] - dungeon[i][n - 1], 1);
+        // }
 
-	public static int calculateMinimum(int[][] arr, int cr, int cc, int[][] dp) {
-		if (cr == arr.length || cc == arr[0].length) {
-			return Integer.MAX_VALUE;
-		}
-		if (dp[cr][cc] != -1) {
-			return dp[cr][cc];
-		}
-		if (cr == arr.length - 1 && cc == arr[0].length - 1) {
-			return arr[cr][cc] > 0 ? 1 : 1 - arr[cr][cc];
-		}
-		int left = calculateMinimum(arr, cr, cc + 1, dp);
-		int right = calculateMinimum(arr, cr + 1, cc, dp);
-		int p = Math.min(left, right) - arr[cr][cc];
+        // for (int j = n - 2; j >= 0; j--) {
+        //     dungeon[m - 1][j] = Math.max(dungeon[m - 1][j + 1] - dungeon[m - 1][j], 1);
+        // }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int down = 0;
+                int right = 0;
+                if(i == m-1 && j == n-1)continue;
+                if (i == m - 1) {
+                    dungeon[i][j] = Math.max(dungeon[i][j + 1] - dungeon[i][j], 1);
+                } else if (j == n - 1) {
+                    dungeon[i][j] = Math.max(dungeon[i + 1][j] - dungeon[i][j], 1);
 
-		return dp[cr][cc] = p > 0 ? p : 1;
+                } else {
+                    right = Math.max(dungeon[i][j + 1] - dungeon[i][j], 1);
+                    down = Math.max(dungeon[i + 1][j] - dungeon[i][j], 1);
+                    dungeon[i][j] = Math.min(down, right);
+                }
 
-	}
-
+                
+            }
+        }
+        return dungeon[0][0];
+    }
 }
