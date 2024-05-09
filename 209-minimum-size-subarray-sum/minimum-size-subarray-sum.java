3d4a1879d7fff08b1fe09bcd4c98dddce4fffad0
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-05-09 09:39:41 +0530

    Time: 1 ms (99.89%) | Memory: 57.8 MB (62.40%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -10,7 +10,7 @@ class Solution {
 
             while (sum >= s) {
                 min = Math.min(min, j - i);
-                System.out.println(min);
+                
                 sum -= a[i++];
             }
         }
