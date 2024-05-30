f4deb0ca27c0cfef8fe058bfbcd135bcd08c296a
thebrijesh <103484600+thebrijesh@users.noreply.github.com>
2024-05-30 11:27:58 +0530

    Time: 140 ms (6.17%) | Memory: 45.1 MB (7.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,35 +1,47 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(board[i][j] != '.'){
-                    for(int k = 0; k < board[i].length; k++){
-                        if(board[i][j] == board[i][k] && j != k ){
-                            return false;
-                        }
-                    }
-
-                    for(int k = 0; k < board.length; k++){
-                        if(board[k][j] == board[i][j] && i != k ){
-                            return false;
-                        }
+        display(board);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    for (int k = 0; k < board.length; k++) {
+                        
+                            if ((k != j && board[i][k] == num) || (k != i && board[k][j] == num)) {
+                                return false;
+                            }
+                        
                     }
 
                     int x = i / 3 * 3;
                     int y = j / 3 * 3;
 
-                    for(int l = x; l < x + 3; l++){
-                        for(int m = y; m < y + 3; m++){
-                            if(board[i][j] == board[l][m] && (i != l || j != m) ){
+                    for (int l = x; l < x + 3; l++) {
+                        for (int m = y; m < y + 3; m++) {
+                            if ((l != i || m != j) && num == board[l][m]) {
                                 return false;
                             }
                         }
                     }
-
                 }
-
             }
         }
         return true;
+    }
+
+    public void display(char[][] board) {
+        int count = 0;
+        int count1 = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+                System.out.print(board[i][j] + " ");
+                count++;
+
+                System.out.print("| ");
+
+            }
+            System.out.println();
+        }
     }
 }
