fbeb175cabc7a9d5f1e48e8646decf1507efbcfb
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-06-10 17:32:08 +0530

    Time: 69 ms (16.09%) | Memory: 45.1 MB (32.97%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,36 +1,24 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        HashSet<List<Integer>> set = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        solve(map,set,new ArrayList<>(),nums,0);
-        for(List<Integer> l : set){
-            ans.add(l);
-        }
-        return ans;
+        Set<List<Integer>> result = new HashSet<>();
+        permuteUnique(nums, new ArrayList<>(), new HashMap<Integer, Integer>(), result);
+        return new ArrayList<>(result);
     }
 
-    public void solve(HashMap<Integer, Integer> map, HashSet<List<Integer>>ans , List<Integer> list, 
-                        int[] nums, int idx){
-        if(idx == nums.length){
-            
-                ans.add(new ArrayList(list));
-            
+    private void permuteUnique(int[] nums, List<Integer> combo, Map<Integer, Integer> indexes, Set<List<Integer>> result) {
+        if (combo.size() == nums.length) {
+            result.add(new ArrayList<>(combo));
             return;
-        }
-        for(int i = 0; i < nums.length; i++){
-            if(map.get(nums[i]) > 0){
-                map.put(nums[i],map.getOrDefault(nums[i],0)-1);
-                list.add(nums[i]);
-                solve(map,ans,list,nums,idx+1);
-                list.remove(list.size()-1);
-                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-
+        } 
+            for (int i = 0; i < nums.length; i++) {
+                if (!indexes.containsKey(i)) {
+                    combo.add(nums[i]);
+                    indexes.put(i, 1);
+                    permuteUnique(nums, combo, indexes, result);
+                    combo.remove(combo.size() - 1);
+                    indexes.remove(i);
+                }
             }
-
-        }
+        
     }
 }
