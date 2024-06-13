44295e1cae07bc92ef92c708f410686850a11eb7
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-06-13 14:15:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (66.94%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        list.add(new ArrayList<>());
-        list.get(0).add(1);
-
-        for(int i = 1; i < numRows; i++){
-            list.add(new ArrayList<>());
-            for(int j = 0; j < list.get(i-1).size()+1; j++){
-                if(j == 0 || j == list.get(i-1).size()){
-                    list.get(i).add(1);
-                }else{
-                    list.get(i).add(list.get(i-1).get(j) + list.get(i-1).get(j-1) );
-                }
+        List<List<Integer>> res = new ArrayList<>();  // This will store the final result
+        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();  // This will store each row
+            int n = 1;  // The first value in each row is always 1
+            
+            for (int j = 0; j <= i; j++) {
+                list.add(n);  // Add the current value to the list
+                n = n * (i - j) / (j + 1);  // Compute the next value in the row
             }
+            
+            res.add(list);  // Add the completed row to the result
         }
-
-        return list;
+        
+        return res;  
     }
 }
