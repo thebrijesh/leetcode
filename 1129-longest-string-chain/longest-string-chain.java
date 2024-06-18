d52189b3026ca68d48d4c7c1457562b2b38af85c
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-06-18 11:37:16 +0530

    Time: 52 ms (48.34%) | Memory: 43.9 MB (89.21%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -20,39 +20,26 @@ class Solution {
             words[i] = arr[i].str;
         }
 
-        int[][] dp = new int[words.length + 1][words.length + 1];
-        for (int[] arrh : dp) {
-            Arrays.fill(arrh, -1);
-        }
-
-        return dfs(words, 0, -1, dp);
-
-    }
-
-    public int dfs(String[] str, int idx, int pre, int[][] dp) {
-        if (idx >= str.length)
-            return 0;
-        if (pre != -1 && dp[idx][pre] != -1) {
-            return dp[idx][pre];
-        }
-
-        int take = Integer.MIN_VALUE;
-
-        if (pre == -1 || str[pre].length() < str[idx].length()) {
-            if (pre == -1){
-                take = 1 + dfs(str, idx + 1, idx, dp);
-            }else if(str[pre].length() + 1 == str[idx].length() && isPred(str[pre], str[idx])) {
-                take = 1 + dfs(str, idx + 1, idx, dp);
+        int[] array = new int[words.length];
+        Arrays.fill(array,1);
+        int max = 0;
+        for(int i = 0; i < words.length; i++){
+            for(int j = 0; j <= i; j++){
+                if(words[j].length() +1  == words[i].length()){
+                    if(isPred(words[j],words[i])){
+                        array[i] = Math.max(array[i],array[j]+1);
+                    }
+                    
+                }
             }
 
+            max = Math.max(array[i],max);
         }
-        int skip = dfs(str, idx + 1, pre, dp);
-        if (pre != -1) {
-            return dp[idx][pre] = Math.max(skip, take);
-        }
-        return Math.max(skip, take);
+        return max;
+
     }
 
+    
     public boolean isPred(String str1, String str2) {
         int n = str2.length();
         int i = 0;
