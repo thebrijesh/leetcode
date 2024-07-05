e457e4999ac4a1a0191306ff4f3afb435fd69cc6
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-07-05 08:16:39 +0530

    Time: 10 ms (26.74%) | Memory: 65.3 MB (24.91%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,29 +1,47 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int first = Integer.MAX_VALUE, last = 0, prev_val = head.val, min_d = Integer.MAX_VALUE;
-        for (int i = 0; head.next != null; i++) {
-            if ((prev_val < head.val && head.val > head.next.val) ||
-                    (prev_val > head.val && head.val < head.next.val)) {
-                if (last != 0)
-                    min_d = Math.min(min_d, i - last);
-                first = Math.min(first, i);
-                last = i;
+        ListNode curr = head.next;
+        ListNode prev  = head;
+        int count = 1;
+        ArrayList<Integer> arrlis = new ArrayList<>();
+        while(curr != null && curr.next != null)
+        {
+            if(curr.val > prev.val && curr.val > curr.next.val)
+            {
+                arrlis.add(count);
             }
-            prev_val = head.val;
-            head = head.next;
+            if(curr.val<prev.val && curr.val < curr.next.val)
+            {
+                arrlis.add(count);
+            }
+            prev = prev.next;
+            curr = curr.next;
+            count++;
         }
-        if (min_d == Integer.MAX_VALUE)
-            return new int[] { -1, -1 };
-        return new int[] { min_d, last - first };
+        int[] res = new int[2];
+       
+        Arrays.fill(res,-1);
+        if(arrlis.size() < 2)
+        {
+            return res;
+        }
+        int minres = Integer.MAX_VALUE;
+        for(int i = 0;i<arrlis.size()-1;i++)
+        {
+            minres = Math.min(minres,arrlis.get(i+1)-arrlis.get(i));
+        }
+        res[0] = minres;
+        res[1] = arrlis.get(arrlis.size()-1)-arrlis.get(0);
+        return res;
     }
 }
