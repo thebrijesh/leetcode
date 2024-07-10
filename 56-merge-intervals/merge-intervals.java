b004f33042cbe36d4166c3f4470a7d33e5e6acea
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-07-10 09:01:12 +0530

    Time: 9 ms (55.01%) | Memory: 46.6 MB (31.99%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,56 +1,36 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 1) {
+        if (intervals.length <= 1) {
             return intervals;
         }
 
-        int max = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            max = Math.max(intervals[i][0], max);
-        }
+        // Sort intervals by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        if (max == 0) {
-            return new int[][] { intervals[0] };
-        }
+        // Use a LinkedList to store the merged intervals
+        LinkedList<int[]> mergedIntervals = new LinkedList<>();
 
-        int[] timeTable = new int[max + 1];
-        for (int i = 0; i < intervals.length; i++) {
-            int startTime = intervals[i][0];
-            int endTime = intervals[i][1];
-            timeTable[startTime] = Math.max(endTime + 1, timeTable[startTime]);
-        }
+        // Initialize with the first interval
+        mergedIntervals.add(intervals[0]);
 
-        int resultSize = 0;
-        int currEnd = -1;
-        int currStart = -1;
+        for (int i = 1; i < intervals.length; i++) {
+            int[] lastInterval = mergedIntervals.getLast();
+            int[] currentInterval = intervals[i];
 
-        for (int i = 0; i < timeTable.length; i++) {
-            if (timeTable[i] != 0) {
-                if (currStart == -1) {
-                    currStart = i;
-                }
-                currEnd = Math.max(timeTable[i] - 1, currEnd);
-            }
-            if (currEnd == i) {
-                intervals[resultSize++] = new int[] { currStart, currEnd };
-                currEnd = -1;
-                currStart = -1;
+            // Check if there is an overlap
+            if (lastInterval[1] >= currentInterval[0]) {
+                // Merge the intervals
+                lastInterval[1] = Math.max(lastInterval[1], currentInterval[1]);
+            } else {
+                // No overlap, add the current interval to the list
+                mergedIntervals.add(currentInterval);
             }
         }
 
-        if (currStart != -1) {
-            intervals[resultSize++] = new int[] { currStart, currEnd };
-        }
-
-        if (intervals.length == resultSize) {
-            return intervals;
-        }
-
-        int[][] res = new int[resultSize][];
-        for (int i = 0; i < resultSize; i++) {
-            res[i] = intervals[i];
-        }
-
-        return res;
+        // Convert LinkedList to array
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 }
