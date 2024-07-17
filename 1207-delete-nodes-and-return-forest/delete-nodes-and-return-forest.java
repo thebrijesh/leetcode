2d859603aaa798f287f47e93e0f164751cbc3738
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-07-17 09:42:36 +0530

    Time: 1 ms (97.28%) | Memory: 44.9 MB (26.77%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -15,46 +15,31 @@
  */
 class Solution {
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        List<TreeNode> list = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(int val:to_delete){
-            set.add(val);
+        Set<Integer> toBeDeleteNodes = new HashSet<>();
+        List<TreeNode> result = new ArrayList<>();
+        if(root==null)
+            return result;
+        for(int i: to_delete){
+            toBeDeleteNodes.add(i);
         }
-        if(!set.contains(root.val))list.add(root);
-        solve(list,set,root);
-
-        for(TreeNode roots : list){
-            solve1(set,roots);
-        }
-        return list;
+        delete(root, toBeDeleteNodes, result);
+        if(!toBeDeleteNodes.contains(root.val))
+            result.add(root);
+        return result;
     }
 
-    public void solve(List<TreeNode> list, HashSet<Integer> set,TreeNode root){
-        if(root == null)return;
-
-        if(set.contains(root.val)){
-            if(root.left != null && !set.contains(root.left.val)){
-                list.add(root.left);
-            }
-            if(root.right != null  && !set.contains(root.right.val)){
-                list.add(root.right);
-            }
+    private TreeNode delete(TreeNode root, Set<Integer> toBeDeleteNodes, List<TreeNode> result){
+        if(root==null)
+            return null;
+        root.left = delete(root.left, toBeDeleteNodes, result);
+        root.right = delete(root.right, toBeDeleteNodes, result);
+        if(toBeDeleteNodes.contains(root.val)){
+            if(root.left!=null)
+                result.add(root.left);
+            if(root.right!=null)
+                result.add(root.right);
+            return null;
         }
-
-        solve(list,set,root.left);
-        solve(list,set,root.right);
-    }
-
-    public void solve1(HashSet<Integer> set,TreeNode root){
-        if(root == null)return;
-        if(root.left != null && set.contains(root.left.val)){
-            root.left = null;
-        }
-        if(root.right != null && set.contains(root.right.val)){
-            root.right = null;
-        }
-
-        solve1(set,root.left);
-        solve1(set,root.right);
+        return root;
     }
 }
