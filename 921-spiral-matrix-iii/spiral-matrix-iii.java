06d0705a4d0c2d64f590cb70072a4995694c5036
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-08-08 11:02:55 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (21.96%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,30 +1,59 @@
 class Solution {
+int minX, minY, maxX, maxY, index;
+
+    int[][] result;
+
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] directions = new int[][]{{0,1}, // right - E
-                                        {1,0}, // down - S
-                                        {0,-1}, // left - W
-                                        {-1,0}};// up - N
         
-        int[][] result = new int[rows * cols][2];
-        int steps = 0;
-        int dir = 0;
-        int idx = 1;
-        result[0] = new int[]{rStart,cStart};
-        while(idx < result.length){
-            if(dir == 0 || dir == 2){
-                steps++;
-            }
-            for(int count = 0; count < steps; count++){
-                rStart += directions[dir][0];
-                cStart += directions[dir][1];
+        int n = cols * rows;
+        result = new int[n][];
 
-                if(rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols){
-                    result[idx++] = new int[]{rStart,cStart};
-                }
+        minX = cStart;
+        maxX = cStart + 1;
+        minY = rStart;
+        maxY = rStart;
+        index = 0;
+        result[index++] = new int[]{rStart, cStart};
 
-            }
-            dir = (dir +1) % 4;
+        while (true) {
+            if (minY >= 0){
+                right(Math.max(0, minX + 1), Math.min(cols - 1, maxX));
+            } 
+            maxY++;
+            if (index >= n) break;
+
+            if (maxX < cols) down(Math.max(0, minY + 1), Math.min(rows - 1, maxY));
+            minX--;
+            if (index >= n) break;
+
+            if (maxY < rows) left(Math.min(cols - 1, maxX - 1), Math.max(0, minX));
+            minY--;
+            if (index >= n) break;
+
+            if (minX >= 0) up(Math.min(rows - 1, maxY - 1), Math.max(0, minY));
+            maxX++;
+            if (index >= n) break;
         }
         return result;
+    }
+
+    public void right(int start, int end) {
+        for (int i = start; i <= end; i++) 
+            result[index++] = new int[]{minY, i};
+    }
+
+    public void left(int start, int end) {
+        for (int i = start; i >= end; i--) 
+           result[index++] = new int[]{maxY, i};
+    }
+
+    public void down(int start, int end) {
+        for (int i = start; i <= end; i++) 
+            result[index++] = new int[]{i, maxX};
+    }
+
+    public void up(int start, int end) {
+        for (int i = start; i >= end; i--) 
+           result[index++] = new int[]{i, minX};
     }
 }
