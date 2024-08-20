082f8def75a1978241dd869153cf87aed2fec05c
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-08-20 09:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (67.40%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int even = 0;
-        int odd = 0;
+   
 
-        for(int i = 0; i < piles.length; i++){
-            if(i % 2 == 0){
-                even += piles[i];
-            }else{
-                odd += piles[i];
-            }
-        }
+    
 
         
         return true;
