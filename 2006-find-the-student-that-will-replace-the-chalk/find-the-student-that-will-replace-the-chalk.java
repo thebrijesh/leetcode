ca701b86e86a54193b35f371e8ff9de971c89b4c
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-09-02 07:31:35 +0530

    Time: 2 ms (72.59%) | Memory: 59.8 MB (68.19%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,23 +1,21 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        // Calculate the total sum of chalk requirements
-        long totalChalk = 0;
+       
+        long sum = 0;
         for (int i = 0; i < chalk.length; i++) {
-            totalChalk += chalk[i];
+            sum += chalk[i];
         }
-        
-        // Reduce k by the total sum of chalk to find the remainder
-        k %= totalChalk;
+        k %= sum;
+       
 
-        // Find the student who will need to be replaced
-        for (int i = 0; i < chalk.length; i++) {
-            if (k < chalk[i]) {
+        for(int i = 0; i < chalk.length; i++){
+            if(k - chalk[i] > -1){
+                 k = k - chalk[i];
+            }else {
                 return i;
             }
-            k -= chalk[i];
+           
         }
-        
-        // In case no student is found, return 0 (though this should never happen given the problem constraints)
         return 0;
     }
 }
