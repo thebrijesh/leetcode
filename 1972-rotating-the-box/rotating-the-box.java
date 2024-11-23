a4cfbf14e7e89d2f305aa9620ca00f47753829a6
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2024-11-23 10:44:40 +0530

    Time: 5 ms (100.00%) | Memory: 80.3 MB (5.01%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,32 +1,15 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-       
-        int row = box.length;
-        int col = box[0].length;
-        char[][] ans = new char[col][row];
-        for(int i= 0; i < row; i++){
-            int k = col-1;
-            for(int j = col-1; j >= 0; j--){
-
-                if(box[i][j] == '*'){
-                    k = j-1;
-                }else if(box[i][j] == '#'){
-                    box[i][j] ='.';
-                    box[i][k--] ='#';
-                }
-
-              
-            }
-        }
-        for(int i= 0; i < row; i++){
-            
-            for(int j = 0; j< col; j++){
-
-                ans[j][row-i-1] = box[i][j]; 
-
-              
-            }
-        }
-        return ans;
+        int m = box.length, n = box[0].length;
+		char[][] res = new char[n][m];
+		for (int i = 0; i < m; i++)
+			for (int j = n - 1, k = n - 1; j >= 0; j--) {
+				res[j][m - i - 1] = '.';
+				if (box[i][j] != '.') {
+					k = box[i][j] == '*' ? j : k;
+					res[k--][m - i - 1] = box[i][j];
+				}
+			}
+		return res;
     }
 }
