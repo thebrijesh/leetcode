5d5a6c1fc14ace75b40faebbc770c15fab807d29
Brijesh Shingala <103484600+thebrijesh@users.noreply.github.com>
2025-02-11 09:11:15 +0530

    Time: 1 ms (99.46%) | Memory: 41.9 MB (91.83%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,45 +1,10 @@
 class Solution {
-    public static String removeOccurrences(String s, String part) {
-        StringBuilder stringBuilder = new StringBuilder(s);
-        int start = -1;
-        char[] ch1 = part.toCharArray();
-        for (int i = 0; i < stringBuilder.length(); i++) {
-            if (stringBuilder.charAt(i) == ch1[0]){
-                start = i;
-                break;
-            }
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder(s);
+        while(sb.indexOf(part) != -1) {
+            int idx = sb.indexOf(part);
+            sb.delete(idx, idx + part.length());
         }
-        if (start == -1)return s;
-
-        while (start != -1){
-            int j = start;
-            int k = 0;
-            while (j < stringBuilder.length() && k < ch1.length && stringBuilder.charAt(j) == ch1[k] ){
-                j++;
-                k++;
-            }
-            if(k == part.length()){
-                stringBuilder.delete(start,j);
-                start = -1;
-                for (int i = 0; i < stringBuilder.length(); i++) {
-                    if (stringBuilder.charAt(i) == ch1[0]){
-                        start = i;
-                        break;
-                    }
-                }
-            }else {
-                int s1 = start;
-                start = -1;
-                for (int i = s1+1; i < stringBuilder.length(); i++) {
-                    if (stringBuilder.charAt(i) == ch1[0]){
-                        start = i;
-                        break;
-                    }
-                }
-            }
-        }
-
-
-        return stringBuilder.toString();
+        return sb.toString();
     }
 }
